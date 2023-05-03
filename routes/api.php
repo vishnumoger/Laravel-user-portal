fc07d8ddb488505bc14b5a1075ef5c5653e0c122
vishnumoger <45228441+vishnumoger@users.noreply.github.com>
2023-05-03 11:33:04 +0530

    Update api.php
    Added brief description for middleware

--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,14 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+/*
+middleware is a feature that allows you to filter HTTP requests 
+entering your application. In simple terms, middleware acts as a middleman
+ between a request and your application. It can perform various tasks 
+ such as verifying user authentication, checking user roles 
+ and permissions, validating input data, logging requests, and more
+*/
+
 Route::group(['middleware' => 'api', 'prefix' => 'auth'], function($router) {
     Route::post('/signup', [AuthController::class, 'signup']);
     Route::post('/login', [AuthController::class, 'login']);
